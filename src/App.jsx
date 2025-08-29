07d1faf42ce24477db8b79a02a2b8459638afadd
Calvin Duong <calvinduong1337@gmail.com>
2025-08-29 11:01:55 -0700

    fixed links to route to repo name

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,38 @@ function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="/products/cpu" element={<CpuPage />} />
-      <Route path="/products/cpu-cooler" element={<CoolerPage />} />
-      <Route path="/products/motherboard" element={<MotherboardPage />} />
-      <Route path="/products/memory" element={<MemoryPage />} />
-      <Route path="/products/storage" element={<StoragePage />} />
-      <Route path="/products/graphics-card" element={<GpuPage />} />
-      <Route path="/products/power-supply" element={<PsuPage />} />
-      <Route path="/products/case" element={<CasePage />} />
+      <Route
+        path="/calvins-computer-creator/products/cpu"
+        element={<CpuPage />}
+      />
+      <Route
+        path="/calvins-computer-creator/products/cpu-cooler"
+        element={<CoolerPage />}
+      />
+      <Route
+        path="/calvins-computer-creator/products/motherboard"
+        element={<MotherboardPage />}
+      />
+      <Route
+        path="/calvins-computer-creator/products/memory"
+        element={<MemoryPage />}
+      />
+      <Route
+        path="/calvins-computer-creator/products/storage"
+        element={<StoragePage />}
+      />
+      <Route
+        path="/calvins-computer-creator/products/graphics-card"
+        element={<GpuPage />}
+      />
+      <Route
+        path="/calvins-computer-creator/products/power-supply"
+        element={<PsuPage />}
+      />
+      <Route
+        path="/calvins-computer-creator/products/case"
+        element={<CasePage />}
+      />
     </Routes>
   );
 }
